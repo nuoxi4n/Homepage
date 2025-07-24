d1fedcaa308dd903a413fdd1b52b345d1b1e98d7
nuoxi4n <nuo_xian@qq.com>
2025-07-25 00:01:21 +0800

    refactor: 移除 Ko-fi 浮动捐赠功能相关代码

--- customScripts.js
+++ customScripts.js
@@ -1,9 +1,4 @@
 // 这里是自定义js文件，你可以编辑任意Script脚本
-kofiWidgetOverlay.draw('nuoxian', {
-    'type': 'floating-chat',
-    'floating-chat.donateButton.text': 'Support Me',
-    'floating-chat.donateButton.background-color': '#00b9fe',
-    'floating-chat.donateButton.text-color': '#fff'
-});
+
 // 自托管 API
 var meting_api='https://api.nxvav.cn/api/music/?server=:server&type=:type&id=:id&auth=:auth&r=:r';
